e76ee4868e78e1f482bc0f079101e91af1e9eacc
Stepan Riha <github@nonplus.net>
2015-05-12 23:00:10 -0500

    [Prerelease] Bumped version number

--- angular-ui-router-title.js
+++ angular-ui-router-title.js
@@ -3,7 +3,7 @@
  *
  * @link https://github.com/nonplus/angular-ui-router-title
  *
- * @license angular-ui-router-title v0.0.1
+ * @license angular-ui-router-title v0.0.2
  * (c) Copyright Stepan Riha <github@nonplus.net>
  * License MIT
  */
@@ -15,16 +15,30 @@ angular.module("ui.router.title", ["ui.router"])
 	.run(["$rootScope", "$timeout", "$state", function($rootScope, $timeout, $state) {
 
 		$rootScope.$on("$stateChangeSuccess", function() {
-			var title = $state.$current.locals.globals.$title;
-			if(angular.isFunction(title)) {
-				title = title();
-			}
+			var title = getTitleValue($state.$current.locals.globals.$title);
 			if(title) {
 				$timeout(function() {
 					$rootScope.$title = title;
 				});
 			}
+
+			$rootScope.$breadcrumbs = [];
+			var state = $state.$current;
+			while(state) {
+				if(state.resolve && state.resolve.$title) {
+					$rootScope.$breadcrumbs.unshift({
+						title: getTitleValue(state.locals.globals.$title),
+						state: state.self.name,
+						stateParams: state.locals.globals.$stateParams
+					})
+				}
+				state = state.parent;
+			}
 		});
+
+		function getTitleValue(title) {
+			return angular.isFunction(title) ? title() : title;
+		}
 
 	}]);
 
